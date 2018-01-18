0a289a28f59552a7a70e551aaf014e8382f8bfb8
Mohammad Nourinik <mohammad@inpin.co>
2018-01-18 16:16:32 +0330

    Resolve issue #1, #2, and #3

--- Mainlist.php
+++ Mainlist.php
@@ -24,8 +24,20 @@ if(isset($_GET['done'])) {
 <head>
     <title>My todo list</title>
     <style>
-        .done {
+        .done1 {
             background-color: #e6e64a;
+        }
+        .done2 {
+            background-color: #e64839;
+        }
+        .done3 {
+            background-color: #aa93e6;
+        }
+        .done4 {
+            background-color: #5de640;
+        }
+        .done5 {
+            background-color: #36e6bb;
         }
     </style>
 </head>
@@ -34,25 +46,39 @@ if(isset($_GET['done'])) {
 <h2>My todo list</h2>
 <ul> <!-- unordered list -->
     <?php
-        $result = $connection->query("select * from todos");
+        $query = "select * from todos";
+        if(isset($_GET['filter'])) {
+            if ($_GET['filter'] == 'todo') {
+                $query .= " where is_done = 0"; // where clause
+            } elseif ($_GET['filter'] == 'done') {
+                $query .= " where is_done = 1";
+            }
+        }
+        $result = $connection->query($query);
 
         if($result->num_rows > 0) {
             while ($row = $result->fetch_assoc()) {
-                if($row['is_done'] == 1) {
-                    echo "<li class='done'>"; // halari ke done shode
+                if($row['is_done']) {
+                    echo "<li class='done" . rand(1, 5) . "'>"; // halari ke done shode
                 } else {
                     echo "<li>"; // halate aadi
                 }
                 echo $row['name'];
-                echo "<a href=\"Mainlist.php?done="
-                    . $row['id']
-                    . "\">Done</a>";
+                if (!$row['is_done']) {
+                    echo "<a href=\"Mainlist.php?done="
+                        . $row['id']
+                        . "\">Done</a>";
+                }
                 echo "</li>";
             }
         }
     ?>
 </ul>
-
+<div>
+    [<a href="Mainlist.php?filter=all">All</a>] <br/>
+    [<a href="Mainlist.php?filter=todo">Todo</a>] <br/>
+    [<a href="Mainlist.php?filter=done">Done</a>] <br/>
+</div>
 <form method="post">
     <input name="todo" placeholder="New item">
     <button>Add</button>
